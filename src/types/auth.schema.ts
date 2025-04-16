9952cd9004e8799d02a55b173beed4eb6d99d0ff
ItsLuyenTran <minhluyen.tran0702@gmail.com>
2025-04-16 10:49:31 +0700

    Add login form component with Google login support, including UI elements and form validation using Zod. Introduce reusable UI components for buttons, cards, forms, inputs, and labels.

---
+++ src/types/auth.schema.ts
@@ -0,0 +1,53 @@
+import { RoleValues } from '@/constant/type'
+import z from 'zod'
+
+export const LoginBody = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6).max(100)
+  })
+  .strict()
+
+export type LoginBodyType = z.TypeOf<typeof LoginBody>
+
+export const LoginResponse = z.object({
+  data: z.object({
+    accessToken: z.string(),
+    refreshToken: z.string(),
+    account: z.object({
+      id: z.number(),
+      name: z.string(),
+      email: z.string(),
+      role: z.enum(RoleValues)
+    })
+  }),
+  message: z.string()
+})
+
+export type LoginResponseType = z.TypeOf<typeof LoginResponse>
+
+export const RefreshTokenBody = z
+  .object({
+    refreshToken: z.string()
+  })
+  .strict()
+
+export type RefreshTokenBodyType = z.TypeOf<typeof RefreshTokenBody>
+
+export const RefreshTokenResponse = z.object({
+  data: z.object({
+    accessToken: z.string(),
+    refreshToken: z.string()
+  }),
+  message: z.string()
+})
+
+export type RefreshTokenResponseType = z.TypeOf<typeof RefreshTokenResponse>
+
+export const LogoutBody = z
+  .object({
+    refreshToken: z.string()
+  })
+  .strict()
+
+export type LogoutBodyType = z.TypeOf<typeof LogoutBody>
